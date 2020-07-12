97448c1ccf0dcdfec162ec78e84bee1f6e722a5e
michaldobiezynski <dobiezynski@gmail.com>
2020-07-12 21:31:28 +0100

    onwards

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-/**
- * @format
- */
-
+// import a library to help create a component
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+import Header from './src/components/header';
+
+// create a component
+const App = () => {
+  return <Header />;
+};
+
+// render thjat component
+AppRegistry.registerComponent('rnAlbums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+// import a library to help create a component
+import React from 'react';
+import {Text} from 'react-native';
+
+// create a component
+const Header = () => {
+  return <Text>Albums</Text>;
+};
+
+export default Header;
